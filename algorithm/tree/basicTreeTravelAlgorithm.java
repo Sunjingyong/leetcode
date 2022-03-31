e3b0240023830d2e0c0df7d428f70444fe10d4ea
sunjingyong <874562939@qq.com>
2022-03-31 20:16:35 +0800

    二叉树的基础遍历算法

---
+++ algorithm/tree/basicTreeTravelAlgorithm.java
@@ -0,0 +1,21 @@
+package tree;
+
+public class basicTreeTravelAlgorithm {
+
+
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+
+
+
+}
